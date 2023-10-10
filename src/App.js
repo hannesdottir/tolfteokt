37de6a2a9853a5a20b2a284ffefa6cbaed617ec5
Maria Hannesdottir <mariaberghannesdottir@gmail.com>
2023-10-10 17:46:43 +0200

    new

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>12.okt</p>
       </header>
     </div>
   );
