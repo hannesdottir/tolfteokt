26b8afa559eb207ced7f4f687474184c0067e1c0
tob <tob@mac.com>
2023-10-14 16:21:13 +0200

    testa

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>12.okt.</p>
+        <p>Testing 12.okt.</p>
       </header>
     </div>
   );
